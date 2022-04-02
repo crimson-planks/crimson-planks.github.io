fa356172950b0ff92a758cb6b3376bff495628b9
crimson-planks <koreaminecraftmonster@gmail.com>
2022-04-02 17:02:35 -0600

    EEEEE

--- MarioV3/download/js/main.js
+++ MarioV3/download/js/main.js
@@ -1,3 +1,4 @@
 function DownloadButtonOnClick(){
+    window.open("https://bit.ly/3LzINbV");
     window.open("https://youtu.be/QMW4AqbuSGg");
 }
--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
     alert("Now we're getting to rickroll phase 2 :D");
-    window.open("https://crimson-planks.github.io/MarioV3/download");
+    window.open("https://crimson-planks.github.io/MarioV3/download/en-US");
 }
