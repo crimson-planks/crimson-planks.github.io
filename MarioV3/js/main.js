7aab68477fc2fe84493f26f49fa008cb70afaed9
crimson-planks <koreaminecraftmonster@gmail.com>
2022-07-08 21:53:44 +0900

    Update main.js

--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
-    alert("Now we're getting to rickroll phase 2 :D");
+    alert("Download Mario Multiverse v7.20 free");
     window.open("https://crimson-planks.github.io/MarioV3/download/en-US");
 }
