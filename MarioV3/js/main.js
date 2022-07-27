051ddaf7c8e550b29065878da86dadad88a5a599
crimson-planks <koreaminecraftmonster@gmail.com>
2022-07-27 22:37:49 +0900

    Update main.js
    changed version from v7.20 to v7.21

--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
-    alert("Download Mario Multiverse v7.20 free");
+    alert("Download Mario Multiverse v7.21 free");
     window.open("https://crimson-planks.github.io/MarioV3/download/en-US");
 }
