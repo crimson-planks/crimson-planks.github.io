b3da5df1de8ec3afba7939c931bb91aa580a9b60
crimson-planks <koreaminecraftmonster@gmail.com>
2022-08-06 15:45:26 -0500

    aaaaaaaaa
    t!rate Johanna
    0

--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
     alert("Download Mario Multiverse v7.21 free");
-    window.open("https://crimson-planks.github.io/MarioV3/");
+    window.open("https://youtu.be/roEkrBkSyls");
 }
