607658b61ebf8e208a36ac37875e735d7eb4851a
crimson-planks <koreaminecraftmonster@gmail.com>
2022-08-06 15:41:12 -0500

    blah Spider sanic
    spider sanic has a crush on Johanna!!!!

--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
     alert("Download Mario Multiverse v7.21 free");
-    window.open("https://crimson-planks.github.io/MarioV3/download/");
+    window.open("https://crimson-planks.github.io/MarioV3/");
 }
