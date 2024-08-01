1aafd3d2ebeeea5f44d4909f7b50d452d2e9564f
crimson-planks <koreaminecraftmonster@gmail.com>
2024-08-01 15:01:44 -0600

    Added v8

--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
-    alert("Download Mario Multiverse v7.21 free");
-    window.open("https://youtu.be/roEkrBkSyls");
+    alert("Download Mario Multiverse for free");
+    window.open("https://crimson-planks.github.io/MarioMultiverseDownloadFreeNoViruses/V8");
 }
