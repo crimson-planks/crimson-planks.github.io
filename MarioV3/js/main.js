3e8ae273e8c3eb4304c04bb550444e3416e54761
crimson-planks <koreaminecraftmonster@gmail.com>
2024-08-05 17:54:17 -0600

    Fixed issue

--- MarioV3/js/main.js
+++ MarioV3/js/main.js
@@ -1,4 +1,4 @@
 function DownloadButtonOnClick(){
     alert("Download Mario Multiverse for free");
-    window.open("https://crimson-planks.github.io/MarioMultiverseDownloadFreeNoViruses/V8");
+    window.open("https://crimson-planks.github.io/MarioMultiverseDownloadFreeNoViruses/en-US");
 }
